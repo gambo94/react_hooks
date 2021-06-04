2f90cf814494bd9a3517ad9a79ba284f87d66520
gambo94 <fedigambo94@gmail.com>
2021-06-04 18:29:40 +0200

    uploading navbar with navlink

--- src/components/09_useContext/AppRouter.js
+++ src/components/09_useContext/AppRouter.js
@@ -2,8 +2,11 @@ import React from 'react';
 import {
     BrowserRouter as Router,
     Switch,
-    Route
+    Route,
+    Redirect
 } from 'react-router-dom';
+import { NavBar } from './NavBar';
+
 import { AboutScreen } from './AboutScreen';
 import { LoginScreen } from './LoginScreen';
 import { HomeScreen } from './HomeScreen';
@@ -13,11 +16,12 @@ export const AppRouter = () => {
     return (
         <Router>
             <div>
-                
+                <NavBar />
                 <Switch>
                     <Route exact path="/" component={HomeScreen} />
                     <Route exact path="/about" component={AboutScreen} />
                     <Route exact path="/login" component={LoginScreen} />
+                    <Redirect to="/" />
 
                 </Switch>
             </div>
